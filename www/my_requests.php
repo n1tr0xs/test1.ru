d25b5a62a023435c9e009619435d6f36fa6727dc
n1tr0xs <niko4pok@gmail.com>
2023-05-08 15:44:23 +0300

    a

--- www/my_requests.php
+++ www/my_requests.php
@@ -8,10 +8,11 @@ $uid = $_SESSION['uid'];
 $result = $conn->query("SELECT * from requests where user_id=$uid");
 $rows = $result->fetch_all(MYSQLI_ASSOC);
 if($rows){
+  echo "Ваши заявки:";
   foreach ($rows as $row) {
     print_request_for_user($row);
   }
 }
 else{
-  echo "No requests";
+  echo "У вас нет заявок.";
 }
